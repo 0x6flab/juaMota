85d32d0daeae4811a6772730c625d1463b208ab8
0x6f736f646f <blackd0t@protonmail.com>
2022-10-09 14:25:22 +0300

    Add backend
    Signed-off-by: 0x6f736f646f <blackd0t@protonmail.com>

---
+++ backend/startup.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get upgrade
+
+apt-get install -y docker docker-compose make git
+
+git config --global user.email "blackd0t@protonmail.com"
+git config --global user.name "0x6f736f646f"
